d3f4cb2123070b2d00da23cef814fa9401bf96ce
Christian Savu <christian.savu@student.fh-kiel.de>
2023-10-28 00:18:44 +0200

    chore: clean up debug method

--- apps/proj1-db-api/src/index.ts
+++ apps/proj1-db-api/src/index.ts
@@ -4,11 +4,6 @@ import { DatabaseUtils } from "./utils/database-utils";
 const database = new Database('assets/db.sqlite');
 const databaseUtils = new DatabaseUtils();
 
-database.get(
-    'SELECT RANDOM() % 100 as result',
-    (_, res) => console.log(res)
-);
-
 databaseUtils.createTable(
     database,
     "items",
