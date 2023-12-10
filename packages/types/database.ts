faa158b6048c1ba9c87131e728cbeb96d28d5ea8
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-12-10 21:42:44 +0100

    feat(types): add column type and expand database
    The database type now has functions for table metadata

--- packages/types/database.ts
+++ packages/types/database.ts
@@ -31,21 +31,77 @@ export interface Database {
   /**
    * Connects to the database.
    *
-   * @param options The options for configuring the database connection.
+   * @param options - The options for configuring the database connection.
    *
    * @returns A promise that resolves when the connection is established.
    */
   connect: (options: DatabaseOptions) => Promise<void>;
+
   /**
    * Closes the connection to the database.
    *
    * @returns A promise that resolves when the connection is closed.
    */
   closeConnection: () => Promise<void>;
+
   /**
    * Checks whether the connection to the database is open.
    *
    * @returns True if the connection is open, false otherwise.
    */
   isConnectedToDatabase: () => boolean;
+
+  getSchemas(): Promise<string[]>;
+
+  /**
+   * Gets the tables in the database.
+   *
+   * @returns A promise that resolves with the tables in the database.
+   */
+  getTables(): Promise<string[]>;
+
+  /**
+   * Creates a table in the database.
+   *
+   * @param name - The name of the table.
+   * @param columns - The columns of the table.
+   *
+   * @returns A promise that resolves when the table is created.
+   */
+  createTable: (name: string, columns: Column[]) => Promise<void>;
+
+  /**
+   * Deletes a table from the database.
+   *
+   * @param name - The name of the table.
+   *
+   * @returns A promise that resolves when the table is deleted.
+   */
+  deleteTable: (name: string) => Promise<void>;
+}
+
+/**
+ * Represents a column in a database table.
+ */
+export interface Column {
+  /**
+   * The name of the column.
+   */
+  name: string;
+  /**
+   * The type of the column.
+   */
+  type?: "TEXT" | "INTEGER" | "REAL" | "BLOB";
+  /**
+   * Whether the column is nullable.
+   */
+  nullable?: boolean;
+  /**
+   * Whether the column is a primary key.
+   */
+  primaryKey?: boolean;
+  /**
+   * Whether the column is unique.
+   */
+  unique?: boolean;
 }
