23240a401b2d145aa37606be642b898c54e9bd42
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-12-11 10:31:59 +0100

    feat(backend): implement new database methods for columns and rows

--- apps/proj3-backend/src/database/sqlite.ts
+++ apps/proj3-backend/src/database/sqlite.ts
@@ -31,6 +31,7 @@ export class SqliteDatabase implements Database {
   }
 
   async connect(options: DatabaseOptions): Promise<void> {
+    // TODO: handle case when already connected to a different database than specified in options
     const dbName: string = options.database ?? DEFAULT_DATABASE_NAME;
 
     this.logger.log(`Connecting to SQLite database ${dbName}...`);
@@ -60,6 +61,7 @@ export class SqliteDatabase implements Database {
 
   async getSchemas(): Promise<string[]> {
     if (!this.db) {
+      this.logger.error("No connection to SQLite database");
       throw new Error("No connection to SQLite database");
     }
 
@@ -72,6 +74,7 @@ export class SqliteDatabase implements Database {
 
   async getTables(): Promise<string[]> {
     if (!this.db) {
+      this.logger.error("No connection to SQLite database");
       throw new Error("No connection to SQLite database");
     }
 
@@ -84,14 +87,17 @@ export class SqliteDatabase implements Database {
 
   async createTable(name: string, columns: Column[]): Promise<void> {
     if (!this.db) {
+      this.logger.error("No connection to SQLite database");
       throw new Error("No connection to SQLite database");
     }
 
     if (columns.length === 0) {
+      this.logger.error("Cannot create table with no columns");
       throw new Error("Cannot create table with no columns");
     }
 
     if (!columns.some((column) => column.primaryKey)) {
+      this.logger.error("Cannot create table without primary key");
       throw new Error("Cannot create table without primary key");
     }
 
@@ -113,11 +119,84 @@ export class SqliteDatabase implements Database {
 
   async deleteTable(name: string): Promise<void> {
     if (!this.db) {
+      this.logger.error("No connection to SQLite database");
       throw new Error("No connection to SQLite database");
     }
 
     this.logger.log(`Dropping table ${name}...`);
     await this.db.run(`DROP TABLE ${name}`);
     this.logger.log(`Dropped table ${name}`);
+  }
+
+  async getColumns(table: string): Promise<Column[]> {
+    if (!this.db) {
+      throw new Error("No connection to SQLite database");
+    }
+
+    return this.db.all<Column[]>(`PRAGMA table_info(${table})`);
+  }
+
+  async addColumn(table: string, column: Column): Promise<void> {
+    if (!this.db) {
+      this.logger.error("No connection to SQLite database");
+      throw new Error("No connection to SQLite database");
+    }
+
+    const type = column.type ?? "TEXT";
+    const nullable = column.nullable ? "" : "NOT NULL";
+    const primaryKey = column.primaryKey ? "PRIMARY KEY" : "";
+    const unique = column.unique ? "UNIQUE" : "";
+
+    this.logger.log(`Adding column ${column.name} to table ${table}...`);
+    await this.db.run(
+      `ALTER TABLE ${table} ADD COLUMN ${column.name} ${type} ${nullable} ${primaryKey} ${unique}`
+    );
+    this.logger.log(`Added column ${column.name} to table ${table}`);
+  }
+
+  async deleteColumn(table: string, column: string): Promise<void> {
+    if (!this.db) {
+      this.logger.error("No connection to SQLite database");
+      throw new Error("No connection to SQLite database");
+    }
+
+    this.logger.log(`Dropping column ${column} from table ${table}...`);
+    await this.db.run(`ALTER TABLE ${table} DROP COLUMN ${column}`);
+    this.logger.log(`Dropped column ${column} from table ${table}`);
+  }
+
+  async getRows(
+    table: string,
+    where?: string,
+    orderBy?: Record<string, "ASC" | "DESC">,
+    limit?: number,
+    offset?: number
+  ): Promise<unknown[]> {
+    if (!this.db) {
+      this.logger.error("No connection to SQLite database");
+      throw new Error("No connection to SQLite database");
+    }
+
+    let query = `SELECT * FROM ${table}`;
+
+    if (where) {
+      query += ` WHERE ${where}`;
+    }
+
+    if (orderBy) {
+      query += ` ORDER BY ${Object.entries(orderBy)
+        .map(([column, order]) => `${column} ${order}`)
+        .join(",")}`;
+    }
+
+    if (limit) {
+      query += ` LIMIT ${limit}`;
+    }
+
+    if (offset) {
+      query += ` OFFSET ${offset}`;
+    }
+
+    return this.db.all<unknown[]>(query);
   }
 }
