7c834549cf27a2763b44490389e245da0914aa44
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-12-14 17:11:44 +0100

    feat(backend): Add getAvailableDatabases() method to SqliteDatabase class

--- apps/proj3-backend/src/database/sqlite.ts
+++ apps/proj3-backend/src/database/sqlite.ts
@@ -1,3 +1,4 @@
+import fs from "node:fs";
 import { Database as Sqlite3Database } from "sqlite3";
 import type { Database as SqliteDatabaseType } from "sqlite";
 import { open } from "sqlite";
@@ -8,10 +9,6 @@ import { Logger } from "../utils/logger";
  * The directory where the SQLite databases are stored.
  */
 const DATABASE_DIRECTORY = "./databases";
-/**
- * The name of the default database.
- */
-const DEFAULT_DATABASE_NAME = "default";
 
 /**
  * Represents a SQLite database connection.
@@ -30,13 +27,23 @@ export class SqliteDatabase implements Database {
     this.logger = new Logger("sqlite");
   }
 
+  static async getAvailableDatabases(): Promise<string[]> {
+    if (!fs.existsSync(DATABASE_DIRECTORY)) {
+      return [];
+    }
+
+    const files = await fs.promises.readdir(DATABASE_DIRECTORY);
+
+    return files.map((file) => file.replace(".db", ""));
+  }
+
   async connect(options: DatabaseOptions): Promise<void> {
     if (this.db) {
       this.logger.error("Already connected to SQLite database");
       throw new Error("Already connected to SQLite database");
     }
 
-    const dbName: string = options.database ?? DEFAULT_DATABASE_NAME;
+    const dbName: string = options.database;
 
     this.logger.log(`Connecting to SQLite database ${dbName}...`);
     this.db = await open({
@@ -153,7 +160,7 @@ export class SqliteDatabase implements Database {
       throw new Error("No connection to SQLite database");
     }
 
-    const type = column.type ?? "TEXT";
+    const type = column.type;
     const nullable = column.nullable ? "" : "NOT NULL";
     const primaryKey = column.primaryKey ? "PRIMARY KEY" : "";
     const unique = column.unique ? "UNIQUE" : "";
