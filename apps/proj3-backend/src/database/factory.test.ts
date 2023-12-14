65276c29523bd09c94ec8e3fe3644ac014bd8f5e
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-12-14 21:36:05 +0100

    test(backend): Add tests for DatabaseFactory class

---
+++ apps/proj3-backend/src/database/factory.test.ts
@@ -0,0 +1,42 @@
+import { DatabaseFactory } from "./factory";
+import { SqliteDatabase } from "./sqlite";
+
+describe("DatabaseFactory", () => {
+  describe("createDatabase", () => {
+    it("should create a new SqliteDatabase instance when the database type is 'sqlite'", async () => {
+      const factory = new DatabaseFactory("sqlite");
+      const options = { database: "test-db" };
+
+      const database = await factory.createDatabase(options);
+
+      expect(database).toBeInstanceOf(SqliteDatabase);
+    });
+
+    it("should throw an error when the database type is unknown", async () => {
+      const factory = new DatabaseFactory("unknown" as unknown as "sqlite");
+      const options = { database: "test-db" };
+
+      await expect(factory.createDatabase(options)).rejects.toThrow(
+        "Unknown database type"
+      );
+    });
+  });
+
+  describe("getAvailableDatabases", () => {
+    it("should return the available databases from SqliteDatabase when the database type is 'sqlite'", async () => {
+      const factory = new DatabaseFactory("sqlite");
+
+      const databases = await factory.getAvailableDatabases();
+
+      expect(databases).toEqual(await SqliteDatabase.getAvailableDatabases());
+    });
+
+    it("should throw an error when the database type is unknown", async () => {
+      const factory = new DatabaseFactory("unknown" as unknown as "sqlite");
+
+      await expect(factory.getAvailableDatabases()).rejects.toThrow(
+        "Unknown database type"
+      );
+    });
+  });
+});
