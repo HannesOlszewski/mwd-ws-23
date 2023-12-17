fd84a1c3b00cffd6811d5e02e703714d332248e4
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-12-17 01:10:14 +0100

    chore(frontend): use angular 17 instead of 16

--- apps/proj3-frontend/src/app/app.component.spec.ts
+++ apps/proj3-frontend/src/app/app.component.spec.ts
@@ -2,9 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proj3-frontend'`, () => {
+  it(`should have the 'proj3-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('proj3-frontend');
@@ -22,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('proj3-frontend app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proj3-frontend');
   });
 });
